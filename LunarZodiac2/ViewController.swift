b43595a78c54d07dc199038d8357369b59ae9e87
Chris Bond <bondcb@bc.edu>
2022-02-08 14:21:55 -0500

    part 1 and 2

--- LunarZodiac2/ViewController.swift
+++ LunarZodiac2/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentYear = 2020
+    var currentImageNumber = 0
+    
+    @IBOutlet weak var yearLabel: UILabel!
+    
+    @IBOutlet weak var animalImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        yearLabel.text = "\(currentYear)"
+        animalImageView.image = UIImage(named: "image\(currentImageNumber)")
     }
 
-
+    @IBAction func leftButtonPressed(_ sender: Any) {
+        currentYear = currentYear - 1
+        yearLabel.text = "\(currentYear)"
+        currentImageNumber = currentImageNumber - 1
+        
+        if currentImageNumber == -1 {
+            currentImageNumber = 11
+        }
+        animalImageView.image = UIImage(named: "image\(currentImageNumber)")
+    }
+    
+    @IBAction func rightButtonPressed(_ sender: Any) {
+        currentYear = currentYear + 1
+        yearLabel.text = "\(currentYear)"
+        
+        currentImageNumber = currentImageNumber + 1
+        if currentImageNumber == 12 {
+            currentImageNumber = 0
+        }
+        animalImageView.image = UIImage(named: "image\(currentImageNumber)")
+        
+    }
 }
 
